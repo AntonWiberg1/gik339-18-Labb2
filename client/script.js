281268160a6704527f2bc325b9a54bdd2b9c0924
AntonWiberg1 <anton.wiberg@live.se>
2024-12-17 14:27:17 +0100

    added comments

--- client/script.js
+++ client/script.js
@@ -1,19 +1,21 @@
 async function fetchUsers() {
+  // uppgift 6. Den skickar en HTTP-get förfrågan till den specificerade adressen /users.
   const response = await fetch("http://localhost:3000/users");
 
-  const users = await response.json();
+  const users = await response.json(); //Detta är en asynkron operation som omvandlar resonse till ett Js-objekt.
 
   return users;
 }
-
-console.log(fetchUsers());
+// vi valde async await då vi kodat i dotnet förut och det är så man hanterar det i dotnet.
 
 async function DisplayUsers() {
+  // uppgift 7. ligger i en egen funktion, kan bara använda await i andra funktioner.
   const areaToPlace = document.getElementById("generatedIdContent");
   const newUl = document.createElement("ul");
   const users = await fetchUsers();
   newUl.classList.add("list-group", "shadow-sm");
 
+  // vi använda classlist för att lägga till klasser på elementen och med hjälp av bootstrap
   users.forEach((user) => {
     const newLi = document.createElement("li");
     newLi.classList.add("list-group-item", "bg-dark", "bg-gradient");
--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
-const express = require("express");
+const express = require("express"); // Den här gör att vi kan använda express. + npm install express sqlite3 --save.
 const server = express();
-const sqlite3 = require("sqlite3").verbose();
+const sqlite3 = require("sqlite3").verbose(); // uppgift 3. npm install express sqlite3 --save.
 
 server
   .use(express.json())
@@ -13,18 +13,19 @@ server
   });
 
 server.listen(3000, () => {
-  // lyssnar efter postförfrågningar på server 3000.
+  // lyssnar på servern samt Samt npm start i terminalen.
   console.log("Server running on http://localhost:3000");
 });
 
 server.get("/users", (req, res) => {
-  const db = new sqlite3.Database("./gik339-labb2.db");
+  const db = new sqlite3.Database("./gik339-labb2.db"); // uppgift 3
   const sql = "SELECT * FROM users";
   db.all(sql, (err, rows) => {
     if (err) {
+      // uppgift 2. Här körs callbackfunktionen. Det är en statusförfrågan och till adress http://localhost:3000/users. Req = request, det är vilken HTTP-förfrågan det är.
       res.status(500).send(err);
     } else {
-      res.send(rows);
+      res.send(rows); // res = response. Tar skickar tillbaka användarna från databasen. Både status och send skickar response.
     }
   });
   db.close();
